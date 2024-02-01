665e897ba72e011bb48f94a51f1a8e8c62ec7ee0
chokkalingam627 <73985448+chokkalingam627@users.noreply.github.com>
2024-02-01 11:57:23 +0000

    features

--- reoutr.py
+++ reoutr.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -9,12 +9,24 @@ def hello_world():
 
 @app.route("/admin")
 def admin():
-    return render_template('admin.html')
+    return render_template('admin.html', items = ['user1', 'user2'])
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.hmtl')
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        print("heyy")
+        if request.form['usernme'] == "admin":
+            if request.form['password'] == '123456':
+                print(request.form['usernme'])
+                return "hii"
+            else:
+                return "Wrong password"
+        else:
+            return "waitt"
 
 @app.route("/profile")
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', items = [['amazon.com', 'user1', '123'],
+                                                    ['facebook.com', 'user2', '456']])
